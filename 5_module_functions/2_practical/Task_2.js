1c404b2b2cfc1e8c5ad1c3fae875619744fed7b6
Daniil Pisarev <SC206VPisarevDaniil@yandex.ru>
2024-05-09 17:42:05 +0300

    test

--- 5_module_functions/2_practical/Task_2.js
+++ 5_module_functions/2_practical/Task_2.js
@@ -3,13 +3,13 @@ let numberGoods = 43
 let promotionalCod = 'ДАРИМ300'
 
 function calculate(cartTotal, numberGoods, promotionalCod = null){
-    let total = cartTotal
-    promotionalCod == 'ДАРИМ300' ? total -= 300 : null
-    numberGoods >= 10 ? total = (total * 95 / 100) : null
-    total > 50000 ? total = 50000 + ((total - 50000) * 80 / 100) : null
-    promotionalCod == 'СКИДКА15' && total >= 20000 ? total = (total * 85 / 100) : null
+  let total = cartTotal;
+  promotionalCod == "ДАРИМ300" ? (total -= 300) : null;
+  numberGoods >= 10 ? (total = (total * 95) / 100) : null;
+  total > 50000 ? (total = 50000 + ((total - 50000) * 80) / 100) : null;
+  promotionalCod == "СКИДКА15" && total >= 20000 ? (total = (total * 85) / 100) : null;
 
-    return total
+  return total;
 }
 
 let result = calculate(cartTotal, numberGoods, promotionalCod)
